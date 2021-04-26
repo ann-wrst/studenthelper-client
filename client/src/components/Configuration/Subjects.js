b223f7eda4a2f5d086f80057783ba0fb8b3ef38b
annwrst <annwrst@gmail.com>
2021-04-26 21:51:40 +0300

    moved subjectslist component to subjects.js

--- client/src/components/Configuration/Subjects.js
+++ client/src/components/Configuration/Subjects.js
@@ -10,6 +10,15 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import SubjectsList from "./SubjectsList";
 import {API_BASE_URL} from "../../constants/api"
 import AddSubject from "./AddSubject";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import DeleteIcon from "@material-ui/icons/Delete";
+import {DialogContentText} from "@material-ui/core";
+import TableContainer from "@material-ui/core/TableContainer";
 
 class Subjects extends Component {
     constructor(props) {
@@ -20,11 +29,26 @@ class Subjects extends Component {
             error_message: '',
             subjects_list: []
         }
-        this.fetchSubjectList=this.fetchSubjectList.bind(this);
+        this.fetchSubjectList = this.fetchSubjectList.bind(this);
+    };
+
+    componentDidMount() {
+        this.fetchSubjectList();
+    }
+
+    handleClickOpen(id) {
+        this.setState({open: true});
+        this.currentID = id;
+        this.added = false;
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+        this.currentID = undefined;
     };
 
     async fetchSubjectList() {
-        let response= await (await fetch(API_BASE_URL + '/subjects', {
+        let response = await (await fetch(API_BASE_URL + '/subjects', {
             credentials: 'include',
             method: 'GET',
             headers: {
@@ -51,14 +75,125 @@ class Subjects extends Component {
         );*/
     }
 
+    rows = [];
+
+    createData(name) {
+        return {name};
+    }
+
+    handleEdit(id, name) {
+        const payload = {
+            "name": name
+        };
+        console.log(id);
+        fetch(API_BASE_URL + `/subjects/${id}`, {
+            credentials: 'include',
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: payload
+        }).then(
+            async response => {
+                let res = await response.json();
+                if (!res.success) {
+                    this.setState(
+                        {error_message: res.error.message}
+                    )
+                } else return res.data;
+            }
+        );
+    }
+
+    async handleDelete(id) {
+        fetch(API_BASE_URL + `/subjects/${id}`, {
+            credentials: 'include',
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(
+            async response => {
+                let res = await response.json();
+                if (!res?.success && typeof res?.error?.message !== "undefined") {
+                    this.setState(
+                        {error_message: res?.error?.message}
+                    )
+                }
+                this.handleClose();
+                this.added = true;
+
+                await this.fetchSubjectList();
+            }
+        );
+    }
+
     render() {
+        this.rows = this.state.subjects_list;
         return (<div style={page_style}>
             <div style={heading_style}><Typography style={subjectsheading_style} variant="h6">
                 Subjects
             </Typography>
                 <AddSubject fetchList={this.fetchSubjectList}> </AddSubject>
-                {console.log("state"+this.state.subjects_list)}
-                <SubjectsList list={this.state.subjects_list}> </SubjectsList>
+                {console.log(this.state.subjects_list)}
+                <TableContainer component={Paper}>
+                    <Table className="table" aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right"> </TableCell>
+                                <TableCell align="right"> </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.rows.map((row) => (
+                                <TableRow key={row.idSubject}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">
+
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Button variant="outlined" color="primary" primary={true} style={edit_style}
+                                                size="small"
+                                                onClick={(event) => this.handleEdit(row.idSubject)}>
+                                            Edit
+                                        </Button>
+                                        {/*<DeleteButton id={row.idSubject} fetchList={this.fetchSubjectList}>*/}
+                                        {/*</DeleteButton>*/}
+                                        <Button variant="outlined" size="small"
+                                                color="secondary" primary={true} startIcon={<DeleteIcon/>}
+                                                onClick={(event) => this.handleClickOpen(row.idSubject)}>
+                                            Delete
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                            <Dialog open={this.state.open} onClose={() => this.handleClose()}
+                                    aria-labelledby="form-dialog-title">
+                                <DialogTitle style={{cursor: 'move'}} id="draggable-dialog-title">
+                                    Delete
+                                </DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText>
+                                        Do you want to delete subject?
+                                    </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={() => this.handleClose()} color="primary">
+                                        Cancel
+                                    </Button>
+                                    <Button onClick={() => this.handleDelete(this.currentID)}
+                                            color="primary">
+                                        Done
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                {/*<SubjectsList list={this.state.subjects_list}> </SubjectsList>*/}
 
             </div>
         </div>);
@@ -68,7 +203,11 @@ class Subjects extends Component {
 const page_style = {
     'display': 'flex',
     'flex-direction': 'column',
-}
+};
+
+const edit_style = {
+    'margin-right': '4px'
+};
 const heading_style = {
     'display': 'flex',
     'flex-direction': 'row',
